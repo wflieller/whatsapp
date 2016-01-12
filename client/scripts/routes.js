5dba91ff01b5b5474307dd6236640b5e42efdffb
Walker Flieller <wflieller@gmail.com>
2016-01-12 11:22:44 -0600

    Set up controller, styled messages

---
+++ client/scripts/routes.js
@@ -0,0 +1,23 @@
+angular
+    .module('Whatsapp')
+    .config(config);
+
+function config($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('tab', {
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'client/templates/tabs.html'
+        })
+        .state('tab.chats', {
+            url: '/chats',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'client/templates/chats.html',
+                    controller: 'ChatsCtrl as chats'
+                }
+            }
+        });
+
+    $urlRouterProvider.otherwise('tab/chats');
+}
